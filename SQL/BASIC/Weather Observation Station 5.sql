ba37c0969457d70eb7a95d4383d7d2d39ec96eb1
Ragupathi-D <48428043+Ragupathi-D@users.noreply.github.com>
2021-12-24 13:53:31 +0530

    Create Weather Observation Station 5.sql

---
+++ SQL/BASIC/Weather Observation Station 5.sql
@@ -0,0 +1,31 @@
+/*
+Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+The STATION table is described as follows:
+  Field         Type
+  ID            NUMBER
+  CITY          VARCHAR2(21)
+  STATE         VARCHAR2(2)
+  LAT_N         NUMBER
+  LONG_W        NUMBER
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+Sample Input
+  For example, CITY has four entries: DEF, ABC, PQRS and WXY.
+
+Sample Output 
+  ABC 3
+  PQRS 4 
+
+Explanation
+  When ordered alphabetically, the CITY names are listed as ABC, DEF, PQRS, and WXY, with lengths  and . The longest name is PQRS, but there are  options for shortest named city. Choose ABC, because it comes first alphabetically.
+
+Note
+  You can write two separate queries to get the desired output. It need not be a single query.
+
+*/
+
+(SELECT MIN(CITY), LENGTH(CITY) AS len FROM STATION 
+GROUP BY len ORDER BY len ASC LIMIT 1 )
+UNION ALL
+(SELECT MIN(CITY), LENGTH(CITY) AS len FROM STATION 
+GROUP BY len ORDER BY len DESC LIMIT 1)
