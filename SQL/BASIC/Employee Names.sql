ca42791e1db68ff91d84c556fce7b82f9b0a84e6
Ragupathi-D <48428043+Ragupathi-D@users.noreply.github.com>
2021-12-25 00:43:20 +0530

    Update Employee Names.sql

--- SQL/BASIC/Employee Names.sql
+++ SQL/BASIC/Employee Names.sql
@@ -4,11 +4,11 @@ Write a query that prints a list of employee names (i.e.: the name attribute) fr
 Input Format
 The Employee table containing employee data for a company is described as follows:
 
-Column        Type 
-employee_id   Integer
-name          Sring
-months        Integer
-salary        Integer
+  Column        Type 
+  employee_id   Integer
+  name          Sring
+  months        Integer
+  salary        Integer
 
 where employee_id is an employee's ID number, name is their name, months is the total number of months they've been working for the company, and salary is their monthly salary.
 
